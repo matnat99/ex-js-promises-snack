adbc1da3f4963508ca6889b1924ebcd2644b5402
Mattia Natale <mattianatale07@gmail.com>
2025-04-10 18:29:06 +0200

    feat: add snack 1 bonus

--- script.js
+++ script.js
@@ -18,3 +18,31 @@ function getPostTitle(id) {
 getPostTitle(1)
   .then((title) => console.log(`Il titolo del post è`, title))
   .catch((err) => console.error(err));
+
+/*
+Bonus: Ottieni l'intero post con l'autore
+Crea una funzione getPost(id) che recupera l'intero post. 
+Concatena una seconda chiamata che aggiunge una proprietà user che contiene i dati dell'autore, 
+recuperati dalla chiamata https://dummyjson.com/users/{post.userId}.
+*/
+
+function getPost(id) {
+  return new Promise((resolve, reject) => {
+    fetch(`https://dummyjson.com/posts/${id}`)
+      .then((res) => res.json())
+      .then((post) => {
+        fetch(`https://dummyjson.com/users/${post.userId}`)
+          .then((res) => res.json())
+          .then((user) => {
+            post.user = user;
+            resolve(post);
+          })
+          .catch(reject);
+      })
+      .catch(reject);
+  });
+}
+
+getPost(1)
+  .then((userPost) => console.log(userPost))
+  .catch((err) => console.error(err));
